385b227876fa8cfcb10428604ff5271bd87a0733
masaPokemon <masa.gongon2011@gmail.com>
2024-12-20 11:00:56 +0900

    Update base.js

--- assets/base.js
+++ assets/base.js
@@ -1,4 +1,4 @@
-$(function(){$(window).width()<$(window).height()?($('link[rel=stylesheet]').attr("href","assets/base_h.css?240712"),$(".vmw").hide(),$(".vmh").show()):($('link[rel=stylesheet]').attr("href","assets/base.css?240712"),$(".vmw").show(),$(".vmh").hide())});
+$(function(){$(window).width()<$(window).height()?($('link[rel=stylesheet]').attr("href","assets/base_h.css"),$(".vmw").hide(),$(".vmh").show()):($('link[rel=stylesheet]').attr("href","assets/base.css"),$(".vmw").show(),$(".vmh").hide())});
 zoom();
 $(window).on('resize load',function(){zoom();});
 function zoom(){if($(window).width()<$(window).height()){var a=500;$("html").css("zoom",$(window).width()/a)}else{a=1360;var b=!1;a/750<$(window).width()/$(window).height()&&(b=!0);$("html").css("zoom",b?$(window).height()/750:$(window).width()/a)}}
